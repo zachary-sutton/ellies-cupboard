3161a0ec09a2216e84db9d27b79dbae6e6485258
Zachary Sutton <zachary-sutton@outlook.com>
2020-01-30 18:11:02 +0000

    Updates

---
+++ precache-manifest.0bdf914d058bc883e65d4232ea4613a8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3a9fdf6e7d76d4cafe002dc8e77406",
+    "url": "/ellies-cupboard/index.html"
+  },
+  {
+    "revision": "b9bcbfbf03ee05cbe11c",
+    "url": "/ellies-cupboard/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "db4619edbce07e0eea80",
+    "url": "/ellies-cupboard/static/css/main.14e24598.chunk.css"
+  },
+  {
+    "revision": "b9bcbfbf03ee05cbe11c",
+    "url": "/ellies-cupboard/static/js/2.aec61da8.chunk.js"
+  },
+  {
+    "revision": "af9c41958bfcab2a48903a96dd7576a8",
+    "url": "/ellies-cupboard/static/js/2.aec61da8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db4619edbce07e0eea80",
+    "url": "/ellies-cupboard/static/js/main.cb0db828.chunk.js"
+  },
+  {
+    "revision": "a93a7e7de83e92b99e1c",
+    "url": "/ellies-cupboard/static/js/runtime-main.7b847aba.js"
+  },
+  {
+    "revision": "d58b76075d6c3730c35b1c15b6302e78",
+    "url": "/ellies-cupboard/static/media/depop-bg.d58b7607.jpg"
+  },
+  {
+    "revision": "06c00a7b44316dec34d7aa52c6518e09",
+    "url": "/ellies-cupboard/static/media/landing.06c00a7b.jpg"
+  },
+  {
+    "revision": "7581ba95ee2ba747e112733ee03c4dd1",
+    "url": "/ellies-cupboard/static/media/look-book.7581ba95.jpg"
+  },
+  {
+    "revision": "0fca15cc7c2a5879ea2b7a6f49a2fda5",
+    "url": "/ellies-cupboard/static/media/pf-logo.0fca15cc.png"
+  }
+]);
